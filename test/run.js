2201ac3a0f6d7597d47a8e8b76038cce73525074
AspieSoft <aspiesoftweb@gmail.com>
2022-04-29 15:37:48 -0400

    Updated

--- test/run.js
+++ test/run.js
@@ -1,7 +1,9 @@
-require('./index');
-
 if(process.argv.includes('--github')){
+  require('./index.min');
+
   setTimeout(function(){
     process.exit(0);
   }, 5000);
+}else{
+  require('./index');
 }
