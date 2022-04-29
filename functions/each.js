da443229f488781eb90cec2b13db5bbe3ad10052
AspieSoft <aspiesoftweb@gmail.com>
2022-04-29 14:00:34 -0400

    first commit

---
+++ functions/each.js
@@ -0,0 +1,13 @@
+function main({runFunc, getOpt}){
+  return {
+    name: 'each',
+    func: function(args, content, opts, level, file){
+
+      //todo: setup each statements
+
+      return '';
+    },
+  }
+}
+
+module.exports = main;
---
+++ test.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const {join} = require('path')
+const tml = require('./index')
+
+app.engine('xhtml', tml(app, {template: 'layout', timeout: '3s'}));
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'xhtml');
+
+app.get('/', function (req, res) {
+  res.render('index', {
+    var1: 'this is a test',
+    test: 1,
+    test0: false,
+    test1: true,
+    url: 'https://www.aspiesoft.com',
+  });
+});
+
+// auto set all views to public pages
+tml.renderPages();
+
+app.listen(3000)
